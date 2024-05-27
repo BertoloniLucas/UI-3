93a9b795a58feac7c89710d2ae51539ff2678b0b
Lucas Bertoloni <lucasbertoloni5@gmail.com>
2024-05-27 13:55:56 -0300

    EAC Hecho
    InputField de password y corrobar la misma

---
+++ Assets/Scripts/LoginManager.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI; 
+
+public class LoginManager : MonoBehaviour
+{
+    public InputField passwordUser; 
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void LeerPassword ()
+    {
+        if (passwordUser.text == "Unity2024")
+        {
+            Debug.Log("Acces Granted"); 
+        }
+        else
+        {
+            Debug.Log("Acces Denied");
+        }
+    }
+}
